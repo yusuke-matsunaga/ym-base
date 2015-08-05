7275ac955e1251cd84ed521e8d2509272a8d13d8
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2015-08-05 23:40:41 +0900

    ﻿cmake/ym_use_gtest.cmake を追加

---
+++ cmake/ym_use_gtest.cmake
@@ -0,0 +1,8 @@
+﻿# ym-common に内蔵の gtest を使うための関数
+function ( ym_use_gtest )
+  add_subdirectory (ym-common/gtest-1.7.0)
+  set (GTEST_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/ym-common/gtest-1.7.0/include")
+  set (GTEST_BOTH_LIBRARIES
+    "${PROJECT_BINARY_DIR}/ym-common/gtest-1.7.0/libgtest.a"
+    "${PROJECT_BINARY_DIR}/ym-common/gtest-1.7.0/libgtest_main.a")
+endfunction ()
