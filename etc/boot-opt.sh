57596a30f23b4e9b02e427019ec742aa00e7118d
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2017-02-04 12:04:09 +0900

    etc/boot.sh.in etc/boot-opt.sh を追加した．

---
+++ etc/boot-opt.sh
@@ -0,0 +1,11 @@
+# @file  boot-opt.sh
+# @brief boot.sh に追加で設定する内容を記述するファイル
+# @author Yusuke Matsunaga (松永 裕介)
+#
+# Copyright (C) 2016, 2017 Yusuke Matsunaga (松永 裕介)
+# All rights reserved
+
+# オプション文字列を追加する場合は以下のように
+# 既存のoptions変数に追加すること．
+
+# options="$options -DXXX=YYY"
